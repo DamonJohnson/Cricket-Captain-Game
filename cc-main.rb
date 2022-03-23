278851935d595c6432244b62cb7ed0520dc45b26
damon johnson <damon-johnson@outlook.com>
2022-03-23 23:16:55 +1000

    feasiblity test for wicket_chance

--- cc-main.rb
+++ cc-main.rb
@@ -12,4 +12,22 @@ class Player
 end
 
 pat_cummins = Player.new("Pat Cummins", "Australia", 20, 90)
-p pat_cummins
+joe_root = Player.new("Joe Root", "England", 85, 30)
+
+class Scoreboard
+end
+
+def Combined_Agg(bat_agg, bowl_agg)
+    return bat_agg * bowl_agg
+end
+
+def wicket_chance(bat_skill, bowl_skill, bat_agg, bowl_agg)
+    puts luck = (1+rand(100))
+    bowl = luck * bowl_agg * bowl_skill
+    bat = (bat_skill/bat_agg)
+    return bowl/bat
+end
+
+puts wicket_chance(100, 1, 1, 1)
+
+#  > 99? "He's got him, he's gone!" : "Nice shot"
